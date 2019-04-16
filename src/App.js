168b02c1e7898cb648d7b7d1c7448fb02cd50ec2
Charles <charlesaperry3@gmail.com>
2019-04-16 13:33:08 -0600

    new font and cleaning up code

--- src/App.js
+++ src/App.js
@@ -9,18 +9,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <nav>
-            <Link to="/">Landing</Link>
-            <Link to="/library">Library</Link>
-          </nav>
-          <h1>Bloc Jams</h1>
-        </header>
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
           <Route path="/album/:slug" component={Album} />
         </main>
+        <footer>
+          <nav className="nav-bar">
+            <div className="main-page-link">
+              <Link to="/">MUSEK</Link>
+            </div>
+            <div className="choose-muse-link">
+              <Link to="/library">choose muse</Link>
+            </div>
+          </nav>
+        </footer>
       </div>
     );
   }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,23 +1,17 @@
 import React from 'react';
+import './styling/landing.css';
 
 const Landing = () => (
   <section className="landing">
-    <h1 className="hero-title">Turn the music up!</h1>
-
-      <section className="selling-points">
-        <div className="point">
-          <h2 className="point-title">Choose your music</h2>
-          <p className="point-description">The world is full of music; why should you have to listen to music that someone else chooses?</p>
-        </div>
-        <div className="point">
-          <h2 className="point-title">Unlimited, streaming, ad-free</h2>
-          <p className="point-description">No arbitrary limits. No distractions.</p>
-        </div>
-        <div className="point">
-          <h2 className="point-title">Mobile enabled</h2>
-          <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
-        </div>
-      </section>
+    <div className="muse-title">
+      <h1 className="hero-title">M U S E K</h1>
+    </div>
+    <div className="muse-word">
+      <p className="muse">muse:</p>
+    </div>
+    <div className="muse-definition">
+      <p className="definition">"a state of deep thought or dreamy abstraction"</p>
+    </div>
 
   </section>
 );
