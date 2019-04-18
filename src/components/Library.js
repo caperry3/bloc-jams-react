d0c06015cbaee8b90bf62dd1d28bf6d0cb971eb0
Charles <charlesaperry3@gmail.com>
2019-04-17 22:44:20 -0600

    new songs added and some styling

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
+import './styling/library.css';
+
 
 class Library extends Component {
   constructor(props) {
@@ -14,7 +16,7 @@ class Library extends Component {
         {
           this.state.albums.map( (album, index) =>
             <Link to={`/album/${album.slug}`} key={index}>
-              <img src={album.albumCover} alt={album.title} />
+              <img src={album.albumCover} alt={album.title} className="album-cover"/>
               <div>{album.title}</div>
               <div>{album.artist}</div>
               <div>{album.songs.length} songs</div>
--- src/data/albums.js
+++ src/data/albums.js
@@ -5,11 +5,9 @@ export default [{
   albumCover: '/assets/images/album_covers/01.jpg',
   slug: 'the-colors',
   songs: [
-      { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3', number: 1 },
-      { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3', number: 2 },
-      { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3', number: 3 },
-      { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3', number: 4 },
-      { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3', number: 5 }
+      { title: 'A Really Good Cloak', duration: '209', audioSrc: '/assets/music/A-Really-Good-Cloak.m4a', number: 1 },
+      { title: 'Lovedream', duration: '261', audioSrc: '/assets/music/Lovedream.m4a', number: 2 },
+      { title: 'Meditation', duration: '321', audioSrc: '/assets/music/Meditation.m4a', number: 3 },
   ]
 }, {
     title: 'The Telephone',
@@ -18,10 +16,20 @@ export default [{
     albumCover: '/assets/images/album_covers/02.jpg',
     slug: 'the-telephone',
     songs: [
-      { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3', number: 1 },
-      { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3', number: 2 },
-      { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3', number: 3 },
-      { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3', number: 4 },
-      { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3', number: 5}
+      { title: 'Another Wave from You', duration: '114', audioSrc: '/assets/music/Another-Wave-from-You.m4a', number: 1 },
+      { title: 'Echoes of Mine', duration: '220', audioSrc: '/assets/music/Echoes-of-Mine.m4a', number: 2 },
+      { title: 'Fountains', duration: '82', audioSrc: '/assets/music/Fountains.m4a', number: 3 },
+      { title: 'Intro', duration: '153.14', audioSrc: '/assets/music/Intro.m4a', number: 4 },
+      { title: 'Klaus I Love You', duration: '105', audioSrc: '/assets/music/Klaus-I-Love-You.m4a', number: 5},
+      { title: 'My Tears Are Becoming a Sea', duration: '152', audioSrc: '/assets/music/My-Tears-Are-Becoming-a-Sea.m4a', number: 6 },
+      { title: 'Outro', duration: '247', audioSrc: '/assets/music/Outro.m4a', number: 7 },
+      { title: 'Soon, My Friend', duration: '189', audioSrc: '/assets/music/Soon-My-Friend.m4a', number: 8 },
+      { title: 'Splendor', duration: '308', audioSrc: '/assets/music/Splendor.m4a', number: 9 },
+      { title: 'This Bright Flash', duration: '144', audioSrc: '/assets/music/This-Bright-Flash.m4a', number: 10 },
+      { title: 'Train to Pluton', duration: '76', audioSrc: '/assets/music/Train-to-Pluton.m4a', number: 11 },
+      { title: 'Wait', duration: '343', audioSrc: '/assets/music/Wait.m4a', number: 12 },
+      { title: 'When Will You Come Home', duration: '84', audioSrc: '/assets/music/When-Will-You-Come-Home.m4a', number: 13 },
+      { title: 'Where the Boats Go', duration: '106', audioSrc: '/assets/music/Where-the-Boats-Go.m4a', number: 14 }
+
     ]
 }];
